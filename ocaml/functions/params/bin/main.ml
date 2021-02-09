3e79001f5e21f3af924833cde0439f8d02ae9f88
Bren <skial.bainn@gmail.com>
2021-02-09 14:39:51 +0000

    Add ocaml void/unitless and params functions.

---
+++ ocaml/functions/params/bin/main.ml
@@ -0,0 +1,9 @@
+Printf.printf "Hello, world!\n";;
+
+let two_arity a b =
+    a +. b
+;;
+
+let a = 34564.;;
+let b = 12399.994;;
+Printf.printf "two_arity result is %f\n" (two_arity a b);;
---
+++ ocaml/functions/void/bin/main.ml
@@ -0,0 +1,6 @@
+Printf.printf "Hello, world!\n";;
+let returnless_method () =
+    Printf.printf "Hello from returnless_method\n"
+;;
+
+returnless_method ();;
