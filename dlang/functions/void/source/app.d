d828b3f8be75cced51fe7b1604dc9681be0ed11a
Bren <skial.bainn@gmail.com>
2021-02-09 14:20:23 +0000

    Add haxe and dlang param and void method examples.

---
+++ dlang/functions/void/source/app.d
@@ -0,0 +1,11 @@
+import std.stdio;
+
+void main()
+{
+	writeln("Hello, world!");
+	returnless_method();
+}
+
+void returnless_method() {
+	writeln("Hello from returnless_method");
+}
---
+++ haxe/functions/void/src/Main.hx
@@ -0,0 +1,14 @@
+package ;
+
+class Main {
+
+    public static function main() {
+        Sys.println("Hello, world!");
+        returnless_method();
+    }
+
+    public static function returnless_method() {
+        Sys.println('Hello from returnless_method');
+    }
+
+}
