d08d926fa1c20a0c30e291ec52904b98f9df4a8f
Bren <skial.bainn@gmail.com>
2021-05-10 10:47:01 +0100

    Add mult/ and sub/ simple examples for Haxe, OCaml and DLang.

---
+++ haxe/types/scalar/float/sub/src/Main.hx
@@ -0,0 +1,12 @@
+package ;
+
+class Main {
+
+    public static function main() {
+        var a:Float = 10.0;
+        var b:Float = 1.55;
+        var r:Float = a - b;
+        Sys.println('The value of r is $r');
+    }
+
+}
---
+++ ocaml/types/scalar/float/mult/bin/main.ml
@@ -0,0 +1,5 @@
+let _ = 
+    let a:float = 10.0 in
+    let b:float = 1.55 in
+    let r:float = a *. b in
+    Printf.printf "The value of r is %f" r
